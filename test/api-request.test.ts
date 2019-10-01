e73f923a39a965724c6880ac8da3195eaa0aa55c
Joachim Dalen <me@d3v.no>
2019-10-01 17:11:45 +0200

    Improve api request construction

--- test/api-request.test.ts
+++ test/api-request.test.ts
@@ -1,4 +1,4 @@
-import { ApiRequest, axios } from '../src/api/ApiRequest';
+import { ApiRequest, axios, ApiRequestConfig } from '../src/api/ApiRequest';
 import chai from 'chai';
 var MockAdapter = require('axios-mock-adapter');
 const expect = chai.expect;
@@ -22,11 +22,12 @@ describe('Api Request', () => {
     mock.onGet('/api/users').reply(200, {
       users: [{ id: 1, name: 'John Smith' }]
     });
-    const data = await new ApiRequest(
-      '/api/users',
-      'GET',
-      false
-    ).performRequest();
+    const requestConfig: ApiRequestConfig = {
+      endpoint: '/api/users',
+      method: 'GET',
+      authenticated: false
+    };
+    const data = await new ApiRequest(requestConfig).performRequest();
 
     expect(data).to.be.eql({ users: [{ id: 1, name: 'John Smith' }] });
   });
@@ -35,17 +36,25 @@ describe('Api Request', () => {
     mock.onGet('/api/users?name=john').reply(200, {
       users: [{ id: 1, name: 'John Smith' }]
     });
-    await new ApiRequest('/api/users', 'GET', false, [
-      { key: 'name', value: 'john' }
-    ]).performRequest();
+    const requestConfig: ApiRequestConfig = {
+      endpoint: '/api/users',
+      method: 'GET',
+      authenticated: false,
+      urlParams: [{ key: 'name', value: 'john' }]
+    };
+    await new ApiRequest(requestConfig).performRequest();
 
     expect(mock.history.get[0].url).to.be.eql('/api/users?name=john');
   });
   it('sets url parameters correctly', async () => {
     mock.onGet('/api/users/1').reply(200, { id: 1, name: 'John Smith' });
-    await new ApiRequest('/api/users/:id', 'GET', false, undefined, [
-      { key: ':id', value: '1' }
-    ]).performRequest();
+    const requestConfig: ApiRequestConfig = {
+      endpoint: '/api/users/:id',
+      method: 'GET',
+      authenticated: false,
+      endpointParams: [{ key: ':id', value: '1' }]
+    };
+    await new ApiRequest(requestConfig).performRequest();
     expect(mock.history.get[0].url).to.be.eq('/api/users/1');
   });
 });
